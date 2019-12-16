3a46c1ca05034b485f529141fe42ea7eb984631a
Daniel Moniz <daniel.moniz@gmail.com>
2019-12-15 21:55:08 -0500

    Update parsing tool to use 1v1s. Fix name bug

--- parse_with_carball.py
+++ parse_with_carball.py
@@ -2,11 +2,11 @@ import os
 
 import carball
 
-input_path = "/Users/daniel/dev/projects/rl_replay_data/rlcs_season_7/"
-output_path = "/Users/daniel/dev/projects/rl_replay_data/rlcs_season_7/"
+input_path = "/Users/daniel/dev/projects/rl_replay_data/replay_files/1v1s/"
+output_path = "/Users/daniel/dev/projects/rl_replay_data/parsed_replays/1v1s/"
 
 for filename in os.listdir(input_path):
 	if filename.endswith('.replay'):
-		name = filename[0:filename.find('.')]
+		name = filename[0:filename.find('.replay')]
 		output = output_path + name + '.json'
 		_json = carball.decompile_replay(input_path + filename, output_path=output, overwrite=True)
