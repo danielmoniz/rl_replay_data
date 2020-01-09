66d1da01efd9c48830255ede67c74b3e01f41d3f
Daniel Moniz <daniel.moniz@gmail.com>
2020-01-08 20:27:36 -0500

    Add upgrade of setuptools to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ source env/bin/activate # *nix
 source env/Scripts/activate # Windows
 
 pip install -r requirements.txt
+pip install --upgrade setuptools # needed in some cases for a working Tensorflow
 
 jupyter serverextension enable --py jupyter_http_over_ws
