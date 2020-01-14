2fcbe2e15add4f57fda35410a3a520185c22767a
Daniel Moniz <daniel.moniz@gmail.com>
2020-01-14 07:36:57 -0500

    Minor updates to replay parsing script for better output

--- parse_with_carball.py
+++ parse_with_carball.py
@@ -5,8 +5,14 @@ import carball
 input_path = "./replay_files/1v1s/"
 output_path = "./parsed_replays/1v1s/"
 
-for filename in os.listdir(input_path):
+print(f"Taking replays from {input_path}")
+print(f"Saving replay data into {output_path}")
+
+input_files = os.listdir(input_path)
+for filename in input_files:
 	if filename.endswith('.replay'):
 		name = filename[0:filename.find('.replay')]
 		output = output_path + name + '.json'
 		_json = carball.decompile_replay(input_path + filename, output_path=output, overwrite=True)
+
+print(f"Finished parsing {len(input_files)} replays.")
