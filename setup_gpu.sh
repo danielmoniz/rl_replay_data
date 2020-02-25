a039f93e81154f0c180a4216fc230a974021f0e9
Daniel Moniz <daniel.moniz@gmail.com>
2020-02-24 20:59:42 -0500

    Upgrade to tensorflow v2.0. Discard requirements-gpu

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -1,7 +1,7 @@
 source env/bin/activate # *nix
 source env/Scripts/activate # Windows
 
-pip install -r requirements-gpu.txt
+pip install -r requirements.txt
 pip install --upgrade setuptools # needed in some cases for a working Tensorflow
 
 jupyter serverextension enable --py jupyter_http_over_ws
