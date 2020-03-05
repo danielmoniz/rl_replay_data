0c4376825c365f85b76aedfb67b5560e8ed32a29
Daniel Moniz <daniel.moniz@gmail.com>
2020-03-04 21:49:37 -0500

    Add script for setting up nvidia toolkit on Linux Mint

---
+++ setup_nvidia.sh
@@ -0,0 +1,9 @@
+# Add the package repositories
+#distribution=$(. /etc/os-release; echo $ID$VERSION_ID) # will not work in Linux Mint - use below
+distribution=ubuntu18.04
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
