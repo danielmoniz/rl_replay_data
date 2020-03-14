352bff0e6ef5a334e83137d6dc3bca64d13bc90c
Daniel Moniz <daniel.moniz@gmail.com>
2020-03-14 12:26:44 -0400

    Revert jupyter_colab.sh file

--- jupyter_colab.sh
+++ jupyter_colab.sh
@@ -1,5 +1,4 @@
-docker run --gpus all -it --rm tensorflow/tensorflow:2.0.0-gpu-py3 \
-	jupyter notebook \
-	  --NotebookApp.allow_origin='https://colab.research.google.com' \
-	  --port=8888 \
-	  --NotebookApp.port_retries=0
+jupyter notebook \
+  --NotebookApp.allow_origin='https://colab.research.google.com' \
+  --port=8888 \
+  --NotebookApp.port_retries=0
