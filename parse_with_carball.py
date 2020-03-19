eb1e96e9221c7dcaef8c39a346f0702677062bed
Daniel Moniz <daniel.moniz@gmail.com>
2020-03-18 20:24:33 -0400

    Update replay parser to parse multiple folders

--- parse_with_carball.py
+++ parse_with_carball.py
@@ -2,17 +2,31 @@ import os
 
 import carball
 
-input_path = "./replay_files/1v1s/"
-output_path = "./parsed_replays/"
+num_bad_files = 0
+output_path = "./parsed_replays/" # include trailing slash
+input_paths = [
+	"./replay_files/1v1s/Syracks/",
+	"./replay_files/1v1s/Kevpert/",
+]
 
-print(f"Taking replays from {input_path}")
-print(f"Saving replay data into {output_path}")
+print(f"Saving all replay data into {output_path}")
 
-input_files = os.listdir(input_path)
-for filename in input_files:
-	if filename.endswith('.replay'):
-		name = filename[0:filename.find('.replay')]
-		output = output_path + name + '.json'
-		_json = carball.decompile_replay(input_path + filename, output_path=output, overwrite=True)
+for input_path in input_paths:
+	input_path = os.path.normpath(input_path)
+	print(f"Taking replays from {input_path}/")
+	prefix = os.path.basename(os.path.normpath(input_path)) + '_'
+	print(f"Saving files with prefix '{prefix}'")
 
-print(f"Finished parsing {len(input_files)} replays.")
+	input_files = os.listdir(input_path)
+	for filename in input_files:
+		if filename.endswith('.replay'):
+			name = filename[0:filename.find('.replay')]
+			output = output_path + prefix + name + '.json'
+			try:
+				_json = carball.decompile_replay(f"{input_path}/{filename}", output_path=output, overwrite=True)
+			except:
+				print(f"Something went wrong reading \"{filename}\". Omitting.")
+				num_bad_files += 1
+
+print(f"Finished parsing {len(input_files) - num_bad_files} replays.")
+print(f" --> {num_bad_files} files failed to parse. See above for filesnames.")
